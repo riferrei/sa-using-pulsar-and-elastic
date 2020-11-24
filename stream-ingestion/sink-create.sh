73d360237fc768b08a4d13df6b9dea0fd20fe09d
riferrei <riferrei@riferrei.com>
2020-11-24 14:10:29 -0500

    Fixed elasticsearch connector install

--- stream-ingestion/sink-create.sh
+++ stream-ingestion/sink-create.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-# Creates a new sink connector using a provided archive.
-# You need to manually download the Pulsar I/O connector
-# for Elasticsearch and then rename the downloaded file
-# to `elasticsearch.nar`. To download the latest version
-# of the connector use the following URL:
-
-# https://pulsar.apache.org/en/download
+# Creates a sink connector for Elasticsearch on Pulsar.
 
 $PULSAR_HOME/bin/pulsar-admin --admin-url http://localhost:8080 \
   sinks create --tenant public --namespace default \
-  --archive ${PWD}/elasticsearch.nar --name es_sink \
+  --sink-type elastic_search --name es_sink \
   --sink-config-file ${PWD}/sink-config.yaml \
   --inputs sa-demo
